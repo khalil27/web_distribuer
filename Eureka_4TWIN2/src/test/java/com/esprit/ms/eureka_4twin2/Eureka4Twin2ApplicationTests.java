934b6b214b4d1c1b9e25f3fcc50c288baaf11416
khalil27 <khalil.taallah@esprit.tn>
2025-04-22 17:43:43 +0100

    eureka

---
+++ Eureka_4TWIN2/src/test/java/com/esprit/ms/eureka_4twin2/Eureka4Twin2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esprit.ms.eureka_4twin2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eureka4Twin2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
